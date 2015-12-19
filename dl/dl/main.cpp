985f1182103c0532d9d5e39703d4b76c389a728f
David Lindeque <david@autumn-software.com>
2015-12-19 11:27:11 +0000

    Create first project

---
+++ dl/dl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dl
+//
+//  Created by David Lindeque on 19/12/2015.
+//  Copyright © 2015 David Lindeque. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
